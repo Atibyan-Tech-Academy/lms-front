6102d19508e8ee6a1aa8f0a002aecbbce9e42b41
Olakunle Oluwatobiloba <tboad04@gmail.com>
2025-09-03 10:54:58 +0100

    Slider
    Images and slider construction

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -74,7 +74,11 @@ const Carousel = () => {
         </div>
 
         {/* Slider indicators */}
+<<<<<<< Updated upstream
         <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+=======
+        <div className="absolute  flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+>>>>>>> Stashed changes
           {[...Array(6)].map((_, i) => (
             <button
               key={i}
@@ -89,7 +93,11 @@ const Carousel = () => {
         {/* Slider controls */}
         <button
           type="button"
+<<<<<<< Updated upstream
           className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+=======
+          className="absolute top-0 start-0  flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+>>>>>>> Stashed changes
           data-carousel-prev
         >
           <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
@@ -113,7 +121,11 @@ const Carousel = () => {
 
         <button
           type="button"
+<<<<<<< Updated upstream
           className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+=======
+          className="absolute top-0 end-0  flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+>>>>>>> Stashed changes
           data-carousel-next
         >
           <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
