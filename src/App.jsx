2f0cd75aaf06f866c3ae097131fd40770320f002
Olakunle Oluwatobiloba <tboad04@gmail.com>
2025-09-12 12:18:09 +0100

    Layout creation
    Instructor and Student Layout creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";
+import StudentLayout from "./layouts/StudentLayout";
 import InstructorDashboard from "./pages/InstructorDashboard";
+import InstructorLayout from "./layouts/InstructorLayout";
 import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,6 +19,46 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
+=======
+    <Routes>
+      {/* Landing Page */}
+      <Route path="/" element={<Home />} />
+
+      {/* Public */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/InstructorLayout" element={<InstructorLayout />} />
+      <Route path="/StudentLayout" element={<StudentLayout />} />
+
+
+      {/* Protected */}
+      <Route
+        path="/student"
+        element={
+          <PrivateRoute roleCheck={["STUDENT"]}>
+            <StudentDashboard />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/instructor"
+        element={
+          <PrivateRoute roleCheck={["LECTURER"]}>
+            <InstructorDashboard />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <PrivateRoute roleCheck={["ADMIN"]}>
+            <AdminDashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+>>>>>>> Stashed changes
   );
 }
 
